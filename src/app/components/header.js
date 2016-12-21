4a472a93ef4ef3e4a5387a1cec9c41a98aabc5c8
Mayank Agarwal <mayank.agarwal@cronj.com>
2016-12-21 11:31:44 +0530

    Setup bug free transformer

---
+++ src/app/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div><h1>dsaf</h1></div>
+      );
+   }
+}
+
+export default Header;
+
+ 
+
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-console.log('Hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './components/header';
+
+
+var MyComponent = React.createClass({
+  render: function() {
+    return <div>Hello World</div>;
+  }
+});
+
+
+ReactDOM.render(<MyComponent />, window.document.getElementById("app"));
